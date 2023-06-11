0701ed052d7657048c0f7dcf471fd57210bae17f
Alessandrror <ssandrorivas@gmail.com>
2023-06-11 03:35:29 -0600

    alpinejs dependency installed

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import Alpine from 'alpinejs';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+const AlpineTest = () => {
+  return {
+    greetings: "Hi!",
+    init: function () {
+      this.greetings = 'XD';
+    }
+  };
+};
+
+window.Alpine = Alpine;
+Alpine.data('alpineTest', AlpineTest);
+Alpine.start();
+
+const root = document.getElementById('app');
+root.setAttribute('x-data', 'alpineTest');
+
+root.innerHTML = `
+  <div x-data="alpineTest">
+    <h1 x-text="greetings"></h1>
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
